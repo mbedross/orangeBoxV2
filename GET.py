e0637a7209d0d03ce195b823946a8f890f328441
Manu <mbedross@caltech.edu>
2017-10-19 14:14:35 -0700

    Fixed variable name mismatch

--- GET.py
+++ GET.py
@@ -20,7 +20,7 @@ import math
 
 def relay(relayNumber):
     if relayNumber == "all":
-        for x in range(0, len(header.relays)-1):
+        for x in range(0, len(header.relayALL)-1):
             relayState[x] = os.system('cat |sudo tee /sys/class/gpio/gpio%s/value' %(relayNumber))
     else:
         relayState = os.system('cat |sudo tee /sys/class/gpio/gpio%s/value' %(relayNumber))
